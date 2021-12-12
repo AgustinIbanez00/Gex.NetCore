49895296000af9d20b7fda1d3d0efe1ce2c3b3cb
Agus12344 <48695094+AgustinIbanez00@users.noreply.github.com>
2021-12-11 21:29:48 -0300

    Ordenar enum PreguntaTipo

--- Gex.NetCore/Models/Enums/PreguntaTipo.cs
+++ Gex.NetCore/Models/Enums/PreguntaTipo.cs
@@ -2,19 +2,19 @@
 public enum PreguntaTipo
 {
     /// <summary>
-    /// Sólo se puede contestar con sí o no.
-    /// </summary>
-    CERRADA,
-    /// <summary>
     /// Debe realizar una justificación de la respuesta.
     /// </summary>
-    TEXTO,
+    TEXTO = 0,
+    /// <summary>
+    /// Sólo se puede contestar con sí o no.
+    /// </summary>
+    CERRADA = 1,
     /// <summary>
     /// Se podrán responder con múltiples valores.
     /// </summary>
-    MULTIPLECHOICE,
+    MULTIPLECHOICE = 3,
     /// <summary>
     /// Indica que se puede responder con muchas respuestas.
     /// </summary>
-    MULTIPLESELECTION
+    MULTIPLESELECTION = 4,
 }
